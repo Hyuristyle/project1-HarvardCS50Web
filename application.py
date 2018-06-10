f11ffe13e9d9367c105221212fefb90beb429f2f
Hyuristyle <hyuri.pimentel@gmail.com>
2018-06-10 07:42:37 -0300

    Several small modifications and clean-ups, including using url_for.

--- application.py
+++ application.py
@@ -35,17 +35,22 @@ class Book:
 		self.pub_date = pub_date
 		self.cover = cover
 
-new_book = Book(
-					title = "The Amazing Uga Buga",
-					author = "Oogie Boogie",
-					isbn = "98374283423X",
-					average_rating = 3.5,
-					work_ratings_count = 28,
-					pub_date = 2018,
-					cover = "http://colorlava.com/wp-content/uploads/2012/11/Classic-Red-Book-Cover-520x760.jpg"
-				)
+some_covers = [
+				"https://spark.adobe.com/images/landing/examples/how-to-book-cover.jpg",
+				"https://about.canva.com/wp-content/uploads/sites/3/2015/01/business_bookcover.png",
+				"https://images.gr-assets.com/books/1405581253l/22735855.jpg",
+				"http://cdn.macrumors.com/article-new/2011/08/steve_jobs_book_cover.jpg",
+				"https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781501127625/steve-jobs-9781501127625_hr-back.jpg"
+			]
 
-search_results = [new_book for book in range(8)]
+search_results = []
+
+for i in range(8):
+	try:
+		exec(f"new_book{i} = Book(title = 'Harry Potter and the Philosophers Stone', author = 'Oogie Boogie', isbn = '98374283423X', average_rating = 3.5, work_ratings_count = 28, pub_date = 2018, cover = some_covers[{i}]); search_results.append(new_book{i})")
+
+	except IndexError:
+		exec(f"new_book{i} = Book(title = 'The Amazing Uga Buga', author = 'Oogie Boogie', isbn = '98374283423X', average_rating = 3.5, work_ratings_count = 28, pub_date = 2018, cover = 'http://colorlava.com/wp-content/uploads/2012/11/Classic-Red-Book-Cover-520x760.jpg'); search_results.append(new_book{i})")
 
 # -----------------------------------------------------------------------------------------------------------------------
 # Pages
@@ -59,25 +64,31 @@ def index():
 def home():
 	return render_template("home.html")
 
-@app.route("/Search")
-def search():
-	return render_template("search_results.html", search_results = search_results)
+@app.route("/Search/<string:search_term>")
+def search(search_term):
+	return render_template("search_results.html", search_results = search_results, search_term = search_term)
 
-@app.route("/Book")
-def book():
-	return render_template("book.html", book = new_book)
+@app.route("/Book/<string:isbn>")
+def book(isbn):
+	user_reviewed = False
 
-@app.route("/Book/<isbn>/NewReview")
+	return render_template("book.html", book = search_results[0], user_reviewed = user_reviewed)
+
+@app.route("/Book/<string:isbn>/NewReview")
 def new_review(isbn):
-	return render_template("review_form.html")
+	return render_template("review_submission.html", book = search_results[0])
 
-@app.route("/Book/<isbn>/NewReview/submit")
+@app.route("/Book/<string:isbn>/NewReview/submit")
 def new_review_submit(isbn):
 	return render_template("book.html")
 
-@app.route("/Author/<name>")
+@app.route("/Author/<string:name>")
 def author(name):
-	return render_template("author.html")
+	return render_template("search_results.html")
+
+@app.route("/Year/<int:pub_date>")
+def pub_date(pub_date):
+	return render_template("search_results.html")
 
 # Users
 @app.route("/Register")
@@ -90,16 +101,17 @@ def user_register_submit():
 
 @app.route("/MyReviews")
 def user_reviews():
-	return render_template("my_reviews.html")
+	return render_template("search_results.html")
 
 @app.route("/Settings")
 def user_settings():
-	return render_template("settings.html")
+	return render_template("user_settings.html")
 
 @app.route("/Logout")
 def logout():
-	return render_template("logout.html")
+	return render_template("index.html")
 
+#
 @app.route("/404")
 def content_not_found():
 	return render_template("error_404.html")
@@ -107,9 +119,9 @@ def content_not_found():
 # -----------------------------------------------------------------------------------------------------------------------
 # API
 
-@app.route("/api/<isbn>")
+@app.route("/api/<string:isbn>")
 def api_isbn(isbn):
-	res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "4e2qojOvwwXtmXlzRdQw", "isbns": str(isbn)})
+	res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "4e2qojOvwwXtmXlzRdQw", "isbns": isbn})
 	return str(res.json()) + "<br>" + str(res.json()["books"][0]["isbn"])
 
 #@app.route("api/404")
