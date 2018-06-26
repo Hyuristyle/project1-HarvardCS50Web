f5eb1a87ce9bafadc6e7682dce5dda0a43a4d0ec
Hyuristyle <hyuri.pimentel@gmail.com>
2018-06-26 12:34:59 -0300

    Added login and registration plus added user functionality into users.py.

--- application.py
+++ application.py
@@ -1,39 +1,27 @@
-import os
-
 from flask import Flask, session, render_template, request, redirect, url_for, jsonify
 from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+
+from DB import *
 
 import requests
 
 from books import *
+from users import *
+from reviews import *
+
+import re
 
 app = Flask(__name__)
-
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
 #-----------------------------------------------------------------------------------------------------------------------
 # Fake User (temp)
-class User:
-	def __init__(self, user_id, name, username, password):
-		self.user_id = user_id
-		self.name = name
-		self.username = username
-		self.password = password
 
-fake_user = User(0, "Hyuri Pimentel", "Hyuri.Pimentel", "ugabuga")
+fake_user = User(1, "Hyuri Pimentel", "Hyuristyle", "12345")
 
 #-----------------------------------------------------------------------------------------------------------------------
 # Pages
@@ -58,7 +46,7 @@ def search():
 
 	return render_template("search_results.html", search_results = books, search_term = search_term)
 
-@app.route("/Book/<string:isbn>")
+@app.route("/Book/<string:isbn>", methods=["GET", "POST"])
 def book(isbn):
 	# TODO: Add option to delete a review
 
@@ -67,9 +55,11 @@ def book(isbn):
 	if book_data is None:
 		return render_template("error_404.html", info = {"type": "book", "message": isbn}, search_term = isbn)
 
-	user_reviewed = False
+	reviews = get_book_reviews(isbn)
 
-	return render_template("book.html", book = book_data, user_reviewed = user_reviewed, search_term = book_data["title"])
+	user_review = get_user_review(isbn, fake_user.user_id)
+
+	return render_template("book.html", book = book_data, reviews = reviews, user_review = user_review, search_term = book_data["title"])
 
 @app.route("/Book/<string:isbn>/NewReview")
 def new_review(isbn, text_area = None):
@@ -82,18 +72,22 @@ def new_review(isbn, text_area = None):
 	if book_data is None:
 		return render_template("error_404.html", info = {"type": "book", "message": isbn})
 
-	return render_template("review_submission.html", book = book_data, user = fake_user, text_area = text_area, search_term = book_data["title"])
+	return render_template("review_submission.html", book = book_data, search_term = book_data["title"])
 
 @app.route("/Book/<string:isbn>/NewReview/submit", methods=["POST"])
 def new_review_submit(isbn):
 	rating = request.form.get("rating-value")
 	review = request.form.get("TextArea")
 
-	if rating == "undefined":
-		return redirect(url_for("new_review", isbn = "283472834", text_area = review))
+	user_id = 1
 
-	else:
-		return f"Review to book {isbn} submitted by . <br> Stars: {rating} <br> Review: {review}" #render_template("book.html")
+	add_review(isbn, user_id, rating, review)
+
+	book_data = get_book_data(isbn, cover_size = "medium")
+
+	reviews = get_book_reviews(isbn)
+
+	return redirect(url_for("book", isbn = isbn, search_term = book_data["title"]))
 
 @app.route("/Author/<string:name>")
 def author(name):
@@ -111,13 +105,44 @@ def year(year):
 	return render_template("search_results.html", search_results = books, search_term = year)
 
 # Users
-@app.route("/Register")
-def user_register():
-	return render_template("register.html")
+@app.route("/Login", methods=["POST"])
+def user_login():
+	if request.method != "POST":
+		return render_template("index.html")
+	
+	username = request.form.get("username_login")
+	password = request.form.get("password_login")
 
-@app.route("/Register/submit")
-def user_register_submit():
+	if not authorize_user(username, password):
+		return "Username or Password incorrect. Try again, please!"
+	
+	# TODO:
+	# user_id = get_user_id(username)
+	# start session/set cookies
+
+	# TODO: send notification informing successful login
 	return render_template("home.html")
+
+@app.route("/Register", methods=["POST"])
+def user_register():
+	if request.method != "POST":
+		return render_template("index.html")
+
+	fullname = request.form.get("fullname_register")
+	username = request.form.get("username_register")
+	password = request.form.get("password_register")
+
+	if username_exists(str(username)):
+		return "Username already taken. Choose a different one, please!"
+
+	if not valid_password(password):
+		return "Password must be at least 8 characters long and contain letters, numbers, uppercase, lowercase and symbols."
+
+	add_user(username, fullname, password)
+	# TODO: start session/set cookies
+
+	# TODO: send notification informing successful registration
+	return redirect(url_for("home"))
 
 @app.route("/MyReviews")
 def user_reviews():
@@ -128,7 +153,7 @@ def user_settings():
 	return render_template("user_settings.html")
 
 @app.route("/Logout")
-def logout():
+def user_logout():
 	return render_template("index.html")
 
 #
@@ -147,7 +172,7 @@ def API_isbn(isbn):
 		return jsonify({"error": "isbn not found"}), 422
 
 	author = db.execute("SELECT name FROM authors WHERE id = :author_id LIMIT 1", {"author_id": book_data.author_id}).fetchone()[0]
-	
+
 	return jsonify({
 			"title": book_data["title"],
 			"author": author,
--- users.py
+++ users.py
@@ -1,22 +1,19 @@
 from DB import *
 
-#-----------------------------------------------------------------------------------------------------------------------
+from re import search as regex_search
 
-# Fake User (temp)
-class User:
-	def __init__(self, user_id, name, username, password):
-		self.user_id = user_id
-		self.name = name
-		self.username = username
-		self.password = password
+from passlib.hash import argon2
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Queries
 
 def get_user_id(username):
 	"""
 	Returns id number that matches username.
-	Returns None if no matches found
+	Returns None if no matches found.
 	"""
 
-	user_id = db.execute("SELECT id FROM users WHERE username = :username LIMIT 1", {"username": username}).fetchone()
+	user_id = db.execute("SELECT id FROM users WHERE username = :username LIMIT 1", {"username": str(username)}).fetchone()
 	
 	if user_id is None:
 		return None
@@ -26,7 +23,7 @@ def get_user_id(username):
 def get_user_fullname(user_id):
 	"""
 	Returns fullname that matches user_id.
-	Returns None if no matches found
+	Returns None if no matches found.
 	"""
 
 	user_fullname = db.execute("SELECT fullname FROM users WHERE id = :user_id LIMIT 1", {"user_id": user_id}).fetchone()
@@ -36,14 +33,81 @@ def get_user_fullname(user_id):
 
 	return user_fullname[0]
 
-def get_book_id(isbn):
-	"""
-	Returns id number that matches isbn.
-	Returns None if no matches found
-	"""
-	book_id = db.execute("SELECT id FROM books WHERE isbn = :isbn LIMIT 1", {"isbn": isbn}).fetchone()
+#-----------------------------------------------------------------------------------------------------------------------
+# Insertions
 
-	if book_id is None:
-		return None
+def add_user(username, fullname, password):
+	if username_exists(str(username)):
+		raise Exception("ADD_USER ERROR: Username already exists.")
 
-	return book_id[0]
+	hashed_password = argon2.hash(password = str(password))
+
+	db.execute("INSERT INTO users (username, fullname, password) VALUES (:username, :fullname, :password)",
+										{"username": str(username), "fullname": str(fullname), "password": hashed_password})
+	db.commit()
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def username_exists(username):
+	username_query = db.execute("SELECT * FROM users WHERE username = :username LIMIT 1", {"username": username}).fetchone()
+	
+	if username_query is None:
+		return False
+
+	return True
+
+def valid_password(password):
+	"""
+	Check the strength of "password".
+	Returns a dict indicating the wrong criteria.
+	A password is considered strong if:
+		8 characters length or more;
+		1 digit or more;
+		1 symbol or more;
+		1 uppercase letter or more;
+		1 lowercase letter or more.
+	"""
+	
+	# Checking password length and searching for digits, uppercase, lowercase and symbols, respectively
+	length_error = len(password) < 8
+	digit_error = regex_search(r"\d", password) is None
+	uppercase_error = regex_search(r"[A-Z]", password) is None
+	lowercase_error = regex_search(r"[a-z]", password) is None
+	symbol_error = regex_search(r"\W", password) is None
+
+	if length_error or digit_error or uppercase_error or lowercase_error or symbol_error:
+		return False
+
+	return True
+
+def authorize_user(username, password):
+	"""
+	Returns True if username and password are correct.
+	Returns False otherwise.
+	"""
+
+	user = db.execute("SELECT password FROM users WHERE username = :username LIMIT 1", {"username": str(username)}).fetchone()
+
+	if user is None:
+		return False
+
+	try:
+		if argon2.verify(str(password), user.password):
+			return True
+
+		return False
+
+	except:
+		return False
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Fake User (temp)
+class User:
+	def __init__(self, user_id, name, username, password):
+		self.user_id = user_id
+		self.name = name
+		self.username = username
+		self.password = password
+
+# NOTES:
+# When deleting a user, make sure to delete their reviews before because user_id is referenced in the reviews table.
